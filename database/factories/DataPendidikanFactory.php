12112fdf4d734d5f898d8679599c1f4d861a0769
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2024-01-19 14:18:43 +0700

    create factory method to generate dummy data

--- database/factories/DataPendidikanFactory.php
+++ database/factories/DataPendidikanFactory.php
@@ -17,7 +17,13 @@ class DataPendidikanFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'nisn' => 300 . $this->faker->unique()->randomNumber(7),
+            'nama_lengkap' => $this->faker->name(),
+            'jenis_sekolah' => $this->faker->numberBetween(1, 2),
+            'asal_sekolah' => $this->faker->randomElement(['SMPN 1 Rawamerta', 'SMPN 2 Rawamerta', 'MTsN 3 Karawang', 'MTsN 5 Karawang', 'SMP Nihayatul Amal', 'SMP Annihayah']),
+            'kelas' => $this->faker->randomElement(['9A', '9B', '9C', '9D', '9E', '9F', '9G', '9H']),
+            'nama_ayah' => $this->faker->name(),
+            'nama_ibu' => $this->faker->name(),
         ];
     }
 }
