7a6e046f58e51085f765378513933a021aa6ca66
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2024-01-19 14:19:05 +0700

    call factory model data pendidikan to generate dummy data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\DataPendidikan;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +20,7 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        DataPendidikan::factory(100)->create();
     }
 }
