44a7e84c6623f3e33058a3f15c88290d1ecd5636
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2024-01-19 14:30:02 +0700

    create method for get data siswa

--- app/Http/Controllers/DataPendidikanController.php
+++ app/Http/Controllers/DataPendidikanController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\DataPendidikan;
 use Illuminate\Http\Request;
 
 class DataPendidikanController extends Controller
 {
-    //
+    public function getDataSiswa($nisn)
+    {
+        $data = [
+            'status' => 'success',
+            'message' => 'Data siswa berhasil ditemukan',
+            'siswa' => DataPendidikan::where('nisn', $nisn)->first()
+        ];
+
+        return response()->json($data);
+    }
 }
