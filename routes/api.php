3d155681275081db94431062a5ea4c4eb506709d
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2024-07-03 11:09:17 +0700

    create middleware for authorized endpoint and create logout endpoint inside auth middleware

--- routes/api.php
+++ routes/api.php
@@ -8,4 +8,7 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::post('login', [AuthController::class, 'login']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
