43e5e1315c2e4fb39ff9a1db0913562319682361
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2024-07-03 11:00:19 +0700

    create login controller to handle auth login user

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -2,10 +2,22 @@
 
 namespace App\Http\Controllers\API;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    //
+    public function login(Request $request)
+    {
+        $credentials = $request->only('username', 'password');
+
+        $user = Auth::attempt($credentials);
+        if ($user) {
+            $token = $request->user()->createToken('authToken')->plainTextToken;
+            return response()->json(['token' => $token], 200);
+        }
+
+        return response()->json(['message' => 'Unauthorized'], 401);
+    }
 }
