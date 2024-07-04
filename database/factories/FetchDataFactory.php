d7b7f4d09ea07bbcb697d55280e2479ffcc2274f
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2024-07-04 21:32:40 +0700

    create factory for fetch data class to produce dummy data

--- database/factories/FetchDataFactory.php
+++ database/factories/FetchDataFactory.php
@@ -17,7 +17,38 @@ class FetchDataFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'nisn' => '009' . fake()->unique($reset = true)->numberBetween(1111111, 9999999),
+            'nama' => fake()->firstName($gender = 'male') . ' ' . fake()->lastName(),
+            'jenis_kelamin' => 'L',
+            'tempat_lahir' => 'Karawang',
+            'tanggal_lahir' => fake()->randomElement([2008, 2009, 2010]) . '-' . fake()->month() . '-' . fake()->dayOfMonth(),
+            'pendidikan_terakhir' => 2,
+            'asal_sekolah' => fake()->randomElement([
+                'SMPN 1 Cikampek',
+                'SMPN 2 Cikampek',
+                'SMPN 3 Cikampek',
+                'SMP Al-Islam Cikampek',
+                'SMP IT Al-Muna Cikampek',
+                'SMP Pupuk Kujang',
+                'SMP Taman Siswa Cikampek',
+                'MTs Al-Ishlah',
+                'MTs Al-Mujahidin',
+                'MTs Al-Hurriyah',
+                'MTs Al-Hikmah'
+            ]),
+            'tahun_lulus' => 2024,
+            'kelas' => fake()->randomElement([
+                '9A',
+                '9B',
+                '9C',
+                '9D',
+                '9E',
+                '9F',
+                '9G',
+                '9H',
+                '9I',
+                '9J',
+            ]),
         ];
     }
 }
