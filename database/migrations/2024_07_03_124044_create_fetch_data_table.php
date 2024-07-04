d3b9639661acb200ef2c4e37265cfabadc455de9
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2024-07-04 20:41:02 +0700

    added some fields to fetch data migration table

--- database/migrations/2024_07_03_124044_create_fetch_data_table.php
+++ database/migrations/2024_07_03_124044_create_fetch_data_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('fetch_data', function (Blueprint $table) {
             $table->id();
+            $table->string('nisn')->unique();
+            $table->string('name');
+            $table->enum('jenis_kelamin', ['L', 'P']);
+            $table->string('tempat_lahir');
+            $table->date('tanggal_lahir');
+            $table->tinyInteger('pendidikan_terakhir');
+            $table->string('asal_sekolah');
+            $table->tinyInteger('tahun_lulus');
+            $table->string('kelas');
             $table->timestamps();
         });
     }
