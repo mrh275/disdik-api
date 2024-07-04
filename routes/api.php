283ddfd6aaf87090e23dbe1e0cf3ad57e1cfee85
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2024-07-04 21:45:07 +0700

    added endpoint route to get data siswa

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\FetchDataController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -11,4 +12,5 @@ Route::get('/user', function (Request $request) {
 Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/siswa/{id}', [FetchDataController::class, 'getData']);
 });
