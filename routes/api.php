1bf2896f54fce73879d693558053373d5e03b991
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2024-07-16 20:55:10 +0700

    removed unnecessary route

--- routes/api.php
+++ routes/api.php
@@ -5,9 +5,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\FetchDataController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
